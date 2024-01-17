b022f18ed3dcfd85170a876fb256168f61f43059
JU TE AMO <gouveialuan0@gmail.com>
2024-01-17 18:35:27 -0300

    first commit

---
+++ CalculadoraRankeada.js
@@ -0,0 +1,40 @@
+// Calculadora de partidas Rankeadas
+ let nomedojogador = "Gordaaoo"
+ let niveldojogador = "134"
+ let nivelRankeada = soma(34, 10)
+  console.log ("diga")
+
+ function soma (vitorias_derrotas){
+    let somatorio = 34 - 10
+        return somatorio
+ }
+
+
+ 
+    if (soma < 10){
+        (nivelRankeada = "Ferro")
+ } else if (soma <= 11){
+    (nivelRankeada = "Bronze")
+ } else if (soma <= 20){
+    (nivelRankeada = "Bronze")
+ } else if (soma <= 21){
+    (nivelRankeada = "prata")
+ } else if (soma <= 50){
+    (nivelRankeada = "Prata")
+ } else if (soma <= 51) {
+    (nivelRankeada = "Ouro")
+ } else if (soma <= 80){
+    (nivelRankeada = "Ouro")
+ } else if (soma <= 81){
+    (nivelRankeada = "Diamante")
+ } else if (soma <= 90){
+    (nivelRankeada = "Diamante")
+ } else if (soma <= 91){
+    (nivelRankeada = "Mestre")
+ } else if (soma <= 100){ 
+    (nivelRankeada = "Mestre")
+ } else if (soma >= 101){
+    (nivelRankeada = "Grão Mestre")
+ }
+ 
+console.log ("O" + nomedojogador "esta no level " + niveldojogador "e seu nivel ranqueada é" + nivelRankeada)
